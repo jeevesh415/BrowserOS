df287059a451335af88b855bc7f356575af40c09
Nikhil Sonti <nikhilsv92@gmail.com>
2025-11-10 14:33:49 -0800

    revert: fix update url

--- packages/browseros/chromium_patches/chrome/browser/extensions/extension_management.cc
+++ packages/browseros/chromium_patches/chrome/browser/extensions/extension_management.cc
@@ -1,5 +1,5 @@
 diff --git a/chrome/browser/extensions/extension_management.cc b/chrome/browser/extensions/extension_management.cc
-index ae782891ad341..458b005af6f9c 100644
+index ae782891ad341..02e100fd1a934 100644
 --- a/chrome/browser/extensions/extension_management.cc
 +++ b/chrome/browser/extensions/extension_management.cc
 @@ -14,6 +14,7 @@
@@ -10,31 +10,7 @@ index ae782891ad341..458b005af6f9c 100644
  #include "base/containers/contains.h"
  #include "base/feature_list.h"
  #include "base/functional/bind.h"
-@@ -244,7 +245,22 @@ GURL ExtensionManagement::GetEffectiveUpdateURL(const Extension& extension) {
-         << "Update URL cannot be overridden to be the webstore URL!";
-     return update_url;
-   }
--  return ManifestURL::GetUpdateURL(&extension);
-+
-+  // Get the update URL from the extension's manifest
-+  GURL manifest_update_url = ManifestURL::GetUpdateURL(&extension);
-+
-+  // BrowserOS extension fallback: If a BrowserOS extension doesn't have an
-+  // force-set the BrowserOS CDN update URL so the extension can receive updates
-+  if (manifest_update_url.is_empty() &&
-+      browseros::IsBrowserOSExtension(extension.id())) {
-+    const GURL browseros_update_url(browseros::kBrowserOSUpdateUrl);
-+    LOG(INFO) << "browseros: Extension " << extension.id()
-+              << " missing update_url in manifest, using BrowserOS CDN: "
-+              << browseros_update_url.spec();
-+    return browseros_update_url;
-+  }
-+
-+  return manifest_update_url;
- }
- 
- bool ExtensionManagement::UpdatesFromWebstore(const Extension& extension) {
-@@ -593,6 +609,14 @@ ExtensionIdSet ExtensionManagement::GetForcePinnedList() const {
+@@ -593,6 +594,14 @@ ExtensionIdSet ExtensionManagement::GetForcePinnedList() const {
        force_pinned_list.insert(entry.first);
      }
    }
