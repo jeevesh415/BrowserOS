b465614630e87d252069c1b1e7dea9c070bc4cde
Nikhil Sonti <nikhilsv92@gmail.com>
2025-11-14 15:32:33 -0800

    refactor 10: cli/build.py set to primary

--- packages/browseros/build/common/__init__.py
+++ packages/browseros/build/common/__init__.py
@@ -3,14 +3,12 @@ Common modules for the BrowserOS build system
 """
 
 from .context import BuildContext, ArtifactType
-from .runner import run
 from .config import load_config, merge_config, load_config_or_defaults, expand_env_vars
 from .notify import Notifier, NullNotifier, SlackNotifier, ConsoleNotifier
 
 __all__ = [
     'BuildContext',
     'ArtifactType',
-    'run',
     'load_config',
     'merge_config',
     'load_config_or_defaults',
